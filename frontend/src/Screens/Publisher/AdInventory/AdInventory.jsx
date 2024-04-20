15eb99a651945f430627f8a62d3aa8d47345045b
Manyando Kaine <mkaine@mtc.com.na>
2024-04-20 20:26:46 +0200

    fixed ad tag generation button

--- frontend/src/Screens/Publisher/AdInventory/AdInventory.jsx
+++ frontend/src/Screens/Publisher/AdInventory/AdInventory.jsx
@@ -1,16 +1,16 @@
 const AdInventory = () => {
     let adInventory = [
-        { AdName: 'URL Shortener', AdID: 1232142, format: 'Image', type: 'Pop up', size: '400px x 300px', availableSlots: 13 },
-        { AdName: 'URL Shortener', AdID: 1232143, format: 'Image', type: 'Top banner', size: '1000px x 400px', availableSlots: 7 },
+        { AdName: 'URL Shortener', AdID: 1232142, format: 'Image', type: 'Pop up', size: '400 x 300', availableSlots: 13 },
+        { AdName: 'URL Shortener', AdID: 1232143, format: 'Image', type: 'Top banner', size: '1000 x 400', availableSlots: 7 },
         { AdName: 'URL Shortener', AdID: 4632342, format: 'Image', type: 'Interstitial', size: 'Full Screen', availableSlots: 3 },
         { AdName: 'URL Shortener', AdID: 2432142, format: 'Video', type: 'Left Banner', size: '720p', availableSlots: 10 },
     ]
 
     const generateAdTag = (id) => {
-        const ad = adInventory.find(item => item.AdID === id)
-        if (ad) {
+        const adItem = adInventory.find(item => item.AdID === id)
+        if (adItem) {
             const adTag = `<script type="text/javascript" src="http://localhost:3001/ad?placement=${id}"`
-            alert(`Ad Tag for ${ad.Ad}:\n\nn${adTag}`)
+            alert(`Ad Tag for ${adItem.AdName}:\n\nn${adTag}`)
         }
     }
 
@@ -36,7 +36,7 @@ const AdInventory = () => {
                             <td>{ad.size}</td>
                             <td>{ad.type}</td>
                             <td>{ad.availableSlots}</td>
-                            <td><button className="generateAdButton" onClick={() => generateAdTag(ad.id)}>Generate Ad Tag</button></td>
+                            <td><button className="generateAdButton" onClick={() => generateAdTag(ad.AdID)}>Generate Ad Tag</button></td>
                         </tr>
                     ))}
                 </tbody>
